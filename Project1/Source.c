54b2f27a379c2d95d52dae73c84f4df165cd43e0
Denis Bilik <denis.bilik4@gmail.com>
2018-12-21 17:28:14 +0100

    vytvorenie Main funkcie a pridanie Vstupy.txt

---
+++ Project1/Source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sirka;
+char nacitanie[201];
+
+
+
+int main(void) {
+	// sem napis svoje riesenie
+	int i, j;
+
+	sirka = 0;
+	scanf("%s", &nacitanie);
+}
